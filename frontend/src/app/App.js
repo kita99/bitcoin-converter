2b34e88765ad81e40f8f4c98854f38dabd5045f0
Adrian Dinis <adriandinis99@gmail.com>
2021-04-08 21:17:47 +0100

    Create base markup

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,5 +1,29 @@
-const App = () => (
-  <h1>Hello world</h1>
-);
+const App = () => {
+  return (
+    <>
+      <div className="row">
+        <div className="col">
+          <input type="number" />
+        </div>
+        <div className="col">
+          <input type="text" />
+        </div>
+        <div className="col">
+          <select>
+            <option value="test">Test</option>
+          </select>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <h5>The amount of bitcoins for [currency] [valueinput] </h5>
+        </div>
+        <div className="col">
+          <h1>RESULT</h1>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default App;
