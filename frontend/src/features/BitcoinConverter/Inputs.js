b31d4d2f860e84ee04ad70c9979b93b8cc78099d
Adrian Dinis <adriandinis99@gmail.com>
2021-04-09 19:30:54 +0100

    Add socketio event handlers to Inputs component

--- frontend/src/features/BitcoinConverter/Inputs.js
+++ frontend/src/features/BitcoinConverter/Inputs.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+
+import useSocket from '../../common/socketio/useSocket';
 
 import {
   selectCurrency,
   selectUpdateInterval,
   selectValue,
   setCurrency,
+  setResult,
   setUpdateInterval,
   setValue,
 } from './bitcoinConverterSlice';
 
 const BitcoinConverterInputs = () => {
   const dispatch = useDispatch();
+  const socket = useSocket();
 
   const currency = useSelector(selectCurrency);
   const updateInterval = useSelector(selectUpdateInterval);
   const value = useSelector(selectValue);
+
+  useEffect(() => {
+    if (!currency || !value) {
+      return;
+    }
+
+    socket.emit('configure', { currency, updateInterval, value });
+  }, [ socket, currency, updateInterval, value ]);
+
+  useEffect(() => {
+    socket.on('bitcoinAmountUpdate', data => {
+      dispatch(setResult(data.bitcoinAmount));
+    });
+  }, [ socket ]);
 
   return (
       <div className="row">
