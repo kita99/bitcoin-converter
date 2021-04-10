3f881bc08d456f4ba7959f71831d9446e13363f1
Adrian Dinis <adriandinis99@gmail.com>
2021-04-10 15:37:11 +0100

    Style Result component

--- frontend/src/features/BitcoinConverter/Result.js
+++ frontend/src/features/BitcoinConverter/Result.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import styled from '@emotion/styled';
 
 import {
   selectCurrency,
@@ -7,20 +8,36 @@ import {
   selectValue,
 } from './bitcoinConverterSlice';
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Column = styled.div`
+  width: 50%;
+  text-align: center;
+`;
+
+const ResultLabel = styled.div`
+  font-weight: 300;
+  font-size: 18px;
+`;
+
 const BitcoinConverterResult = () => {
   const currency = useSelector(selectCurrency);
   const result = useSelector(selectResult);
   const value = useSelector(selectValue);
 
   return (
-    <div className="row">
-      <div className="col">
-        <h5>The amount of bitcoins for { currency } { value } </h5>
-      </div>
-      <div className="col">
+    <Container>
+      <Column style={ { marginTop: '5%' } }>
+        <ResultLabel>The amount of bitcoins for { currency } { value } </ResultLabel>
+      </Column>
+      <Column>
         <h1>{ result }</h1>
-      </div>
-    </div>
+      </Column>
+    </Container>
   );
 };
 
