f02898a14d4230d6fd09764a1851be939b302e97
Adrian Dinis <adriandinis99@gmail.com>
2021-04-11 14:09:05 +0100

    Add enzyme to the frontend test framework

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import { configure } from 'enzyme'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+
+// Setup enzyme's react adapter
+configure({ adapter: new Adapter() })
